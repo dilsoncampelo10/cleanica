2fd4b76013e47435964aedc5fb57880495ff39ad
dilsoncampelo10 <dilsono316@gmail.com>
2022-08-04 22:37:31 -0300

    Criando models

---
+++ models/Doctor.php
@@ -0,0 +1,67 @@
+<?php  
+
+class Doctor{
+    private $crm;
+    private $name;
+    private $gender;
+    private $specialty;
+    private $phone;
+    private $address;
+    private $password;
+
+    public function getCrm(){
+        return $this->crm;
+    }
+    public function setCrm($crm){
+        $this->crm = $crm;
+    }
+
+    public function getName(){
+        return $this->name;
+    }
+    public function setName($name){
+        $this->cpf = ucwords(trim($name));
+    }
+    
+    public function getGender(){
+        return $this->gender;
+    }
+    public function setGender($gender){
+        $this->cpf = strtoupper(trim($gender));
+    }
+
+    public function getPhone(){
+        return $this->phone;
+    }
+    public function setPhone($phone){
+        $this->phone = $phone;
+    }
+
+    public function getAddress(){
+        return $this->address;
+    }
+    public function setAddress($address){
+        $this->address = ucwords(trim($address));
+    }
+
+    public function getPassword(){
+        return $this->password;
+    }
+    public function setPassword($pass){
+        $this->password = $pass;
+    }
+
+
+}
+
+interface DoctorDao{
+    public function add(Doctor $d);
+
+    public function findAll();
+
+    public function findByCrm($crm);
+
+    public function update(Doctor $d);
+
+    public function delete($crm);
+}
---
+++ models/Employee.php
@@ -0,0 +1,73 @@
+<?php  
+
+class Employee{
+    private $cpf;
+    private $name;
+    private $gender;
+    private $phone;
+    private $address;
+    private $office;
+    private $password;
+
+    public function getCpf(){
+        return $this->cpf;
+    }
+    public function setCpf($cpf){
+        $this->cpf = $cpf;
+    }
+
+    public function getName(){
+        return $this->name;
+    }
+    public function setName($name){
+        $this->cpf = ucwords(trim($name));
+    }
+    
+    public function getGender(){
+        return $this->gender;
+    }
+    public function setGender($gender){
+        $this->cpf = strtoupper(trim($gender));
+    }
+
+    public function getPhone(){
+        return $this->phone;
+    }
+    public function setPhone($phone){
+        $this->phone = $phone;
+    }
+
+    public function getAddress(){
+        return $this->address;
+    }
+    public function setAddress($address){
+        $this->address = ucwords(trim($address));
+    }
+
+    public function getOffice(){
+        return $this->office;
+    }
+    public function setOffice($office){
+        $this->office = ucwords(trim($office));
+    }
+
+    public function getPassword(){
+        return $this->password;
+    }
+    public function setPassword($pass){
+        $this->password = $pass;
+    }
+
+}
+
+interface PatientDao{
+    public function add(Employee $e);
+
+    public function findAll();
+
+    public function findByCpf($cpf);
+
+    public function update(Employee $e);
+
+    public function delete($cpf);
+}
